00a19a6bdf4b2573517ed7729157622db588326d
MoeinLatifi <moeinlatifi7117@gmail.com>
2022-07-20 20:20:51 +0430

    added constants and basic models for test requests

---
+++ app/src/main/java/com/example/accuweather/data/model/FiveDayModel.kt
@@ -0,0 +1,94 @@
+package com.example.accuweather.data.model
+
+
+import com.google.gson.annotations.SerializedName
+
+data class FiveDayModel(
+    @SerializedName("DailyForecasts")
+    val dailyForecasts: List<DailyForecast>,
+    @SerializedName("Headline")
+    val headline: Headline
+) {
+    data class DailyForecast(
+        @SerializedName("Date")
+        val date: String,
+        @SerializedName("Day")
+        val day: Day,
+        @SerializedName("EpochDate")
+        val epochDate: Int,
+        @SerializedName("Link")
+        val link: String,
+        @SerializedName("MobileLink")
+        val mobileLink: String,
+        @SerializedName("Night")
+        val night: Night,
+        @SerializedName("Sources")
+        val sources: List<String>,
+        @SerializedName("Temperature")
+        val temperature: Temperature
+    ) {
+        data class Day(
+            @SerializedName("HasPrecipitation")
+            val hasPrecipitation: Boolean,
+            @SerializedName("Icon")
+            val icon: Int,
+            @SerializedName("IconPhrase")
+            val iconPhrase: String
+        )
+
+        data class Night(
+            @SerializedName("HasPrecipitation")
+            val hasPrecipitation: Boolean,
+            @SerializedName("Icon")
+            val icon: Int,
+            @SerializedName("IconPhrase")
+            val iconPhrase: String
+        )
+
+        data class Temperature(
+            @SerializedName("Maximum")
+            val maximum: Maximum,
+            @SerializedName("Minimum")
+            val minimum: Minimum
+        ) {
+            data class Maximum(
+                @SerializedName("Unit")
+                val unit: String,
+                @SerializedName("UnitType")
+                val unitType: Int,
+                @SerializedName("Value")
+                val value: Double
+            )
+
+            data class Minimum(
+                @SerializedName("Unit")
+                val unit: String,
+                @SerializedName("UnitType")
+                val unitType: Int,
+                @SerializedName("Value")
+                val value: Double
+            )
+        }
+    }
+
+    data class Headline(
+        @SerializedName("Category")
+        val category: String,
+        @SerializedName("EffectiveDate")
+        val effectiveDate: String,
+        @SerializedName("EffectiveEpochDate")
+        val effectiveEpochDate: Int,
+        @SerializedName("EndDate")
+        val endDate: String,
+        @SerializedName("EndEpochDate")
+        val endEpochDate: Int,
+        @SerializedName("Link")
+        val link: String,
+        @SerializedName("MobileLink")
+        val mobileLink: String,
+        @SerializedName("Severity")
+        val severity: Int,
+        @SerializedName("Text")
+        val text: String
+    )
+}
---
+++ app/src/main/java/com/example/accuweather/utils/Constants.kt
@@ -0,0 +1,10 @@
+package com.example.accuweather.utils
+
+object Constants {
+    const val API_KEY = "cHAyEGUGfxJAYg76qAPLhW0WNAGaXbPX"
+    const val FIVE_DAY_ENDPOINT = "forecasts/v1/daily/5day/"
+    const val LOCATION_ENDPOINT = "locations/v1/cities/search/"
+    const val BASE_URL = "https://dataservice.accuweather.com/"
+    const val FORECAST_BASE_URL = "https://dataservice.accuweather.com/forecasts/v1/"
+    const val LOCATION_BASE_URL = "https://dataservice.accuweather.com/locations/v1/"
+}
