41d33f48366a48bdcda799eb11190efa7f07c344
MoeinLatifi <moeinlatifi7117@gmail.com>
2022-07-20 20:29:57 +0430

    added ServiceAPI interface for test request(may change in future)

---
+++ app/src/main/java/com/example/accuweather/data/remote/ServiceAPI.kt
@@ -0,0 +1,24 @@
+package com.example.accuweather.data.remote
+
+import com.example.accuweather.data.model.FiveDayModel
+import com.example.accuweather.data.model.LocationModel
+import com.example.accuweather.utils.Constants
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Path
+import retrofit2.http.Query
+
+interface ServiceAPI {
+
+    @GET(Constants.LOCATION_ENDPOINT)
+    suspend fun getCityLocation(
+        @Query("q") q: String,
+        @Query("apikey") apiKey: String = Constants.API_KEY
+    ): Response<LocationModel>
+
+    @GET(Constants.FIVE_DAY_ENDPOINT)
+    suspend fun getFiveDayForecast(
+        @Path("location key") locationKey: String,
+        @Query("apikey") apiKey: String = Constants.API_KEY
+    ): Response<FiveDayModel>
+}
